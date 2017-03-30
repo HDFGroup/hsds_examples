b64a09f5fd4c2f79858ce1de8f95fa2f3baef162
jreadey <jreadey@hdfgroup.org>
2017-03-29 17:14:38 -0700

    update for writing to hsds

--- cubes/writecube.py
+++ cubes/writecube.py
@@ -1,24 +1,34 @@
+import sys
+import numpy as np
+import logging
+import h5pyd
+
+loglevel = logging.DEBUG
+logging.basicConfig(format='%(asctime)s %(message)s', level=loglevel)
 cube_side = 256
 
 if len(sys.argv) > 1:
-    cube_side = int(sys.argv[1])
+    if sys.argv[1] in ("-h", "--help"):
+        print("Usage: python writecube.py [cube_side]")
+        sys.exit(-1)
+    else:
+        cube_side = int(sys.argv[1])
 
-#filename = "/home/john/load_test/"
-filename = "cube_"
-filename += str(cube_side) + "_" + str(cube_side) + "_" + str(cube_side) + ".h5"
+filename = "/home/john/cubes/"
+filename += "cube_" + str(cube_side) + ".h5"
 
+print("creating domain:", filename)
 f = h5pyd.File(filename, "w") 
 
 print("filename,", f.filename)
 
 print("create dataset")
 
-dset = f.create_dataset('dset', (cube_side, cube_side, cube_side), dtype='f4')
+dset = f.create_dataset('dset', (cube_side, cube_side, cube_side), dtype='f8')
 
 print("name:", dset.name)
 print("shape:", dset.shape)
 print("dset.type:", dset.dtype)
-print("dset.maxshape:", dset.maxshape)
 
 print("writing data...")
 
