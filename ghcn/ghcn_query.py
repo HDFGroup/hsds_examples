b05e460c02a9b8f8cfb4056a2f7de884f52a5e43
jreadey <jreadey@hdfgroup.org>
2017-04-12 10:06:33 -0700

    added NEX examples

---
+++ ghcn/ghcn_query.py
@@ -0,0 +1,17 @@
+import h5pyd as h5py
+import numpy as np
+import sys
+
+
+def main():
+    filepath = "/home/john/sample/ghcn.h5"  # 82 GB  
+    station_id = "US1WAKG0020"  # seattle station
+    f = h5py.File(filepath, 'r', endpoint="http://192.168.1.100:5102", username="john", password="woof")
+    dset = f["/dset"]
+    print("nrows:", dset.shape[0])
+    query = "station == b'{}'".format(station_id)
+    result = dset.read_where(query)
+    print("result: {} rows".format(len(result)))
+    f.close()
+    
+main()
---
+++ tasmax/read_region.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+USE_H5PY = False
+if USE_H5PY:
+    import h5py
+    folder = "/home/john/HDF/data/NEX/"
+else:
+    import h5pyd as h5py
+    folder = "/home/john/NEX/"
+    
+    
+filename = folder + "tasmax_amon_BCSD_historical_r1i1p1_CONUS_CSIRO-Mk3-6-0_199001-199412.nc"
+
+f = h5py.File(filename, 'r')
+dset = f["/tasmax"]
+print("tasmax shape: {}".format(dset.shape))
+print("tasmax chunk layout: {}".format(dset.chunks))
+num_bytes = dset.shape[1] * dset.shape[2] * dset.dtype.itemsize
+print("selection: [0,:,:]")
+print("reading {} bytes".format(num_bytes))
+data = dset[0,:,:]
+print("done!")
+print("min: {}".format(np.min(data)))
+print("max: {}".format(np.max(data)))
+
+f.close()
